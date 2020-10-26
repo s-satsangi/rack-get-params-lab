b38328d215d67931ce5965f8ec6d45a69a6d940f
s-satsangi <s.satsangi@gmail.com>
2020-10-25 23:54:56 -0400

    A tribute to copy and paste.  The last line of the readme oughta say @@cart and not @@items

--- app/application.rb
+++ app/application.rb
@@ -1,13 +1,13 @@
 class Application
 
-  @@items = ["Apples","Carrots","Pears"]
+  @@cart = ["Apples","Carrots","Pears"]
 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/items/)
-      @@items.each do |item|
+    if req.path.match(/cart/)
+      @@cart.each do |item|
         resp.write "#{item}\n"
       end
     elsif req.path.match(/search/)
@@ -21,10 +21,41 @@ class Application
   end
 
   def handle_search(search_term)
-    if @@items.include?(search_term)
-      return "#{search_term} is one of our items"
+    if @@cart.include?(search_term)
+      return "#{search_term} is in your cart"
     else
       return "Couldn't find #{search_term}"
     end
   end
 end
+
+
+# class Application
+
+#   @@items = ["Apples","Carrots","Pears"]
+
+#   def call(env)
+#     resp = Rack::Response.new
+#     req = Rack::Request.new(env)
+
+#     if req.path.match(/items/)
+#       @@items.each do |item|
+#         resp.write "#{item}\n"
+#       end
+#     elsif req.path.match(/search/)
+
+#       search_term = req.params["q"]
+
+#       if @@items.include?(search_term)
+#         resp.write "#{search_term} is one of our items"
+#       else
+#         resp.write "Couldn't find #{search_term}"
+#       end
+
+#     else
+#       resp.write "Path Not Found"
+#     end
+
+#     resp.finish
+#   end
+# end 
